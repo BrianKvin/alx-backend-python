a95e2865062e1d8939b49f5739500c3684ce5148
briankvin <briankvin@gmail.com>
2025-06-20 19:02:00 +0300

    renamed dir

---
+++ Django-Middleware-0x03/Django-Middleware-0x03/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from chats.auth import CustomTokenObtainPairView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('messaging_app.chats.urls')), 
+]
